b1ea22056f2005a5399a4b23a81220d4715c3690
Marat Chukmarov <ttx013@gmail.com>
2023-12-24 22:22:26 +0300

    added row and column numbers

--- main.jsx
+++ main.jsx
@@ -50,18 +50,84 @@ class DataGrid {
   }
   viewToGrid({ x, y }) {
     return {
-      x: this.size - x - 1 + this.offsetX,
-      y: this.size - y - 1 + this.offsetY,
+      x: this.XToGrid(x),
+      y: this.YToGrid(y),
     };
   }
+  YToGrid(y) {
+    return this.size - y - 1 + this.offsetY
+  }
+  XToGrid(x) {
+    return  this.size - x - 1 + this.offsetX
+  }
+
   render() {
     const subgrid = new GridView(this.grid).calculateWindow(
       [this.offsetX, this.offsetY],
       this.size,
       "top-left"
     );
-    renderGrid(subgrid, this.cellSize, this.dom, this);
+    this.renderGrid(subgrid, this.cellSize, this.dom, this);
   }
+  renderGrid(windowView, cellSize, gridViewElement, grid) {
+    gridViewElement.innerHTML = ""; // Clear previous grid
+    const column_actions = document.createElement("div");
+    column_actions.className = "column-actions-selection";
+    column_actions.classList.add("column-actions-selection");
+    gridViewElement.appendChild(column_actions);
+  
+    const row_actions = document.createElement("div");
+    row_actions.className = "row-actions-selection";
+    row_actions.classList.add("row-actions-selection");
+    gridViewElement.appendChild(row_actions);
+    
+    windowView.forEach((row, rowIndex) => {
+      const rowElement = document.createElement("div");
+      rowElement.classList.add("grid-row");
+      row.forEach((cellValue, columnIndex) => {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("cell");
+        const { x, y } = grid.viewToGrid({ x: columnIndex, y: rowIndex });
+        cellElement.setAttribute("data-x", x);
+        cellElement.setAttribute("data-y", y);
+        cellElement.onclick = () => updateStats({ x, y }, cellValue);
+        cellElement.onmouseover = () => updateHoverStats({ x, y }, cellValue);
+        if (cellValue != 0) cellElement.textContent = cellValue;
+        else cellElement.textContent = "";
+        if (cellValue > 0) {
+          cellElement.style.backgroundColor = "rgb(186 230 253)";
+        } else if (cellValue < 0) {
+          cellElement.style.backgroundColor = "rgb(253 164 175)";
+        }
+  
+        cellElement.style.width = `${cellSize}px`; // Set cell width
+        cellElement.style.height = `${cellSize}px`; // Set cell height
+        rowElement.appendChild(cellElement);
+  
+  
+      });
+  
+      const columnNumberElement=document.createElement("div");
+      columnNumberElement.style.width = `${cellSize}px`; // Set cell width
+      columnNumberElement.style.height = `${cellSize}px`; // Set cell height
+      columnNumberElement.classList.add('column-selection-cell');
+      columnNumberElement.setAttribute("data-x",  grid.XToGrid(rowIndex));
+      columnNumberElement.innerHTML =  grid.XToGrid(rowIndex);
+      column_actions.append(columnNumberElement)
+  
+  
+      const rowNumberElement=document.createElement("div");
+      rowNumberElement.style.width = `${cellSize}px`; // Set cell width
+      rowNumberElement.style.height = `${cellSize}px`; // Set cell height
+      rowNumberElement.classList.add('row-selection-cell');
+      rowNumberElement.setAttribute("data-y",  grid.YToGrid(rowIndex));
+      rowNumberElement.innerHTML =  grid.YToGrid(rowIndex);
+      row_actions.append(rowNumberElement)
+  
+      gridViewElement.appendChild(rowElement);
+    });
+  }
+  
   moveGridLeft() {
     this.offsetX -= 1;
   }
@@ -87,36 +153,7 @@ document.querySelector("#enter-number").addEventListener("change", (e) => {
   grid.clear();
   grid.appendNumberAsBinary(parseInt(e.target.value));
 });
-// Function to render the grid
-function renderGrid(windowView, cellSize, gridViewElement, grid) {
-  gridViewElement.innerHTML = ""; // Clear previous grid
 
-  windowView.forEach((row, rowIndex) => {
-    const rowElement = document.createElement("div");
-    rowElement.classList.add("grid-row");
-    row.forEach((cellValue, columnIndex) => {
-      const cellElement = document.createElement("div");
-      cellElement.classList.add("cell");
-      const { x, y } = grid.viewToGrid({ x: columnIndex, y: rowIndex });
-      cellElement.setAttribute("data-x", x);
-      cellElement.setAttribute("data-y", y);
-      cellElement.onclick = () => updateStats({ x, y }, cellValue);
-      cellElement.onmouseover = () => updateHoverStats({ x, y }, cellValue);
-      if (cellValue != 0) cellElement.textContent = cellValue;
-      else cellElement.textContent = "";
-      if (cellValue > 0) {
-        cellElement.style.backgroundColor = "rgb(186 230 253)";
-      } else if (cellValue < 0) {
-        cellElement.style.backgroundColor = "rgb(253 164 175)";
-      }
-
-      cellElement.style.width = `${cellSize}px`; // Set cell width
-      cellElement.style.height = `${cellSize}px`; // Set cell height
-      rowElement.appendChild(cellElement);
-    });
-    gridViewElement.appendChild(rowElement);
-  });
-}
 // Function to update the stats widget
 function updateStats({ x, y }, value) {
   const cellStats = document.getElementById("cell-stats");
